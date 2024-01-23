ae0f33b23e2089aeeecca03658c37c9140992359
Xavier Solano <89545518+xavismith91@users.noreply.github.com>
2024-01-23 12:51:23 -0600

    230124

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sesiones en  PHP </title>
-    <h1>Bienvenido: <?php $_SESSION['usuario']?></h1>
+    <h1>Login</h1>
+    <br> 
+    <a href="validar.php">Ingresar</a>
+    <br>
+    <br>
 </head>
 <body>
     
